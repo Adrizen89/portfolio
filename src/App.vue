813c12754e8549134df45ad92d75b7f9663a1831
Adrizen89 <adrienberard@hotmail.fr>
2024-10-07 09:44:44 +0200

    Init Project

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import HomePage from '@/views/HomePage.vue'
+</script>
+
+<template>
+  <header></header>
+
+  <main>
+    <HomePage />
+  </main>
+</template>
+
+<style scoped></style>
---
+++ src/views/HomePage.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>My Portfolio</h1>
+</template>
+
+<style lang="css" scoped>
+h1 {
+  background: var(--primary-gradient);
+  -webkit-background-clip: text;
+  color: transparent;
+}
+</style>
